4d51557500b28604e5a8c7e65748d975a6bfa197
muhibtaha <muhibtahaboy42@gmail.com>
2024-10-12 14:30:43 +0300

    iskelet yapıldı

--- muhibtahaboy_hw1/muhibtahaboy_hw1.c
+++ muhibtahaboy_hw1/muhibtahaboy_hw1.c
@@ -1,13 +1,46 @@
 #include <stdio.h>
+#include <math.h>
 
-int N;
+
+#define N 10 /*numbers of points*/
+#define X 0.05 /* percent difference between the closest one and the next closest
+one.*/
+#define EPSILON 0.001 /* to avoid them by perturbing the 0 results*/
 
 int 
     main(void){
 
+long x_of_cluster_1, /*x coordinates*/
+     x_of_cluster_2,
+     x_of_cluster_3,
+
+     y_of_cluster_1, /*y coordinates*/
+     y_of_cluster_2,
+     y_of_cluster_3,
+
+     input_x, /*x and y coordinates which is input*/
+     input_y,
+
+     new_x, /* ? */
+     new_y;
 
 
- printf("Bir x 1gir");
+int  number_of_points_of_cluster_1, /*number of points*/
+     number_of_points_of_cluster_2,
+     number_of_points_of_cluster_3;
+
+
+
+ printf("Give x of new point: ");
+ scanf("%lf", &input_x);
+
+ printf("Give y of new point: ");
+ scanf("%lf", &input_y);
+
+
+
+
+
 
 
 
