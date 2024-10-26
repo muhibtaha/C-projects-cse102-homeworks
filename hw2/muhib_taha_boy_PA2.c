d34fb055870339901d581d4aa9df531b151252b0
muhibtaha <muhibtahaboy42@gmail.com>
2024-10-26 19:17:29 +0300

    artık tek tek kayacak.

--- hw2/muhib_taha_boy_PA2.c
+++ hw2/muhib_taha_boy_PA2.c
@@ -1,110 +1,97 @@
 #include <stdio.h>
-#define X 5 /*size of segments*/
-#define Y 5 /*number of recent segments*/
-#define c 1 /*small constant integer*/
-#define MAX_SIZE c*X*Y /*max size of our array*/
-#define control 230104004 /*control number which is not in input.txt*/
+#define X 4 /* size of segments */
+#define Y 2 /* number of recent segments */
+#define c 1 /* small constant integer */
+#define MAX_SIZE c*X*Y /* max size of our array */
+#define control 230104004 /* control number which is not in input.txt */
 
+int main(void) {
+    FILE *inputFile = fopen("input.txt", "r");
+    FILE *outputFile = fopen("output.txt", "w");
 
+/*error control*/
+    if (inputFile == NULL || outputFile == NULL) {
+        printf("File opening error!\n");
+        return 1;
+    }
 
-
-int 
-    main (void){
-
- FILE *inputFile= fopen("input.txt", "r");
- FILE *outputFile= fopen("output.txt", "w");
-
-
-
-
-int x_count=0, /*to count size of current segment*/
-    y_count=0, /*to count number of remembered segment*/
-    found=0, 
-    nextNumber;
-
+    int x_count = 0; /* to count size of current segment */
+    int y_count = 0; /* to count number of remembered segments */
+    int found = 0;
+    int nextNumber;
     int temp_array[X];
-    int permanent_array[Y][X]; /*an array to keep numbers in the segment and keep segment*/
-    int segment_count_array[Y]= {0};
-        
-int i,p;
-for(i=0; i<X; i++){
-    for ( p = 0; p < Y; p++)
-    {
-permanent_array[p][i]= control ;}; };  /*to sure that all arrays do not have random numbers
-                                                                          we will use it later for possibilty of the fact that
-                                                                                        all given number may be remembered for all input
-                                                                                                                       so out arrays may not be full  */
+    int permanent_array[Y][X]; /* an array to keep numbers in the segment and keep segments */
+    int segment_count_array[Y] = {0};
+
+    /* initialize arrays with control value to ensure no random numbers */
+    int i,p;
+    for (i = 0; i < X; i++) {
+        for (p = 0; p < Y; p++) {
+            permanent_array[p][i] = control;
+        }
+    }
+
+    while (fscanf(inputFile, "%d", &nextNumber) != EOF) {
+        temp_array[x_count] = nextNumber;
+        x_count++;
+
+        if (x_count == X) {
+            x_count = X - 1;  /* we add x-1 new number */
+            found = 0; /* let there has not any same segments ++++*/
+
+            /* Check if this is an existing segment in memory */
+            int j, k;
+            for (j = 0; j < Y; j++) {
+                int match = 1; /*let all numbers match*/
+                for (k = 0; k < X; k++) {
+                    if (temp_array[k] != permanent_array[j][k]) {
+                        match = 0; /* if any number does not match skip that segment */
+                        break;
+                    }
+                }
+                if (match==1) {
+                    segment_count_array[j]++;
+                    found = 1;  /* we have same segment ++++*/
+                    
+                    /*if there is a match with any segment, we do not need to check all segments */
+                    break;
+                }
+            }
+
+            /* if it is a segment that is not in memory, add it as a new segment */
+            if (found==0) {
+                int t=0;
+                while (t < X){    
+                    permanent_array[y_count][t] = temp_array[t]; /* Write to permanent memory */
+                t++;
+                
+                }
+                segment_count_array[y_count] = 1;  /*start to count of new segment and also delete old segment data*/
+                y_count = (y_count + 1) % Y; /*it have to forget except last Y, so we used for that % Y*/
 
 
+            }
 
+            /* shift elements just one */
+            int m;
+            for (m = 1; m < X; m++) {
+                temp_array[m - 1] = temp_array[m];
+            }
+        }
+    }
 
-while (fscanf(inputFile, "%d", &nextNumber) != EOF){
-
-
-                                    temp_array[x_count%X]=nextNumber;
-                                    x_count++;
-                                    if(x_count==X){
-                                        x_count=0;
-                                        found=0; /*a segment counter if all numbers match */
-
-
-                                        /*(if that was not same segment)*/
-                                        int j,k;
-                                            for  (j = 0; j < Y; j++)
-                                            {   
-                                                int match=1;
-                                                    for ( k = 0; k < X; k++)
-                                                    {
-                                                        if(temp_array[k]!=permanent_array[j][k]) {  match=0; /* if any number do not match skip that segment*/
-                                                                                                    break;
-                                                        };
-                                                    };
-
-                                                   if(match==1) {   segment_count_array[j]++; 
-                                                                    found=1;  /*if there are match with any segment we do not need to look all segments*/
-                                                                    break; } 
-                                            };
-                                            
-         
-
-                    /*if it is segment which is not in memory ...*/
-                        if(found==0){
-                            int t;
-                                        for (t = 0; t < X; t++)
-                                        {
-                                            permanent_array[y_count][t]=temp_array[t]; /*write to permanent memory*/
-                                        };
-                                        segment_count_array[y_count]=1; /*start to count of new segment and also delete old segment data*/ 
-                                        y_count = (y_count + 1) % Y; /*it have to forget except last Y, so we used for that % Y*/
-                                        
-
-                        };            
-};
-};
-
- int q,w;
- for ( q = 0; q < Y; q++)
- {
-    if (permanent_array[q][0]!= control){
-
-        for (w = 0; w < X; w++)
-        {
-            fprintf (outputFile, "%d ", permanent_array[q][w]);
-        };
-        
-        fprintf(outputFile, ": %d\n",segment_count_array[q]);
-
-    };
-
- };
- 
-
+int q, w;
+    for (q = 0; q < Y; q++) {
+        if (permanent_array[q][0] != control) {
+            for (w = 0; w < X; w++) {
+                fprintf(outputFile, "%d ", permanent_array[q][w]);
+            }
+            fprintf(outputFile, ": %d\n", segment_count_array[q]);
+        }
+    }
 
     fclose(inputFile);
     fclose(outputFile);
 
     return 0;
-
-
-
-    }
+}
