9903d953d2b4f21a351a8cb635af5ae105c0997e
muhibtaha <muhibtahaboy42@gmail.com>
2024-11-10 20:21:56 +0300

    finished

--- hw3/muhib_taha_boy_PA3.c
+++ hw3/muhib_taha_boy_PA3.c
@@ -2,9 +2,10 @@
 #include <string.h>
 #include <math.h>
 
-#define MAX_LINE_LENGTH 1024  // Satır boyutu (buffer boyutu)
+#define MAX_LINE_LENGTH 1024  
+#define MAX_TERMS 100  /*max size of polyomal*/
 
-/* exponent calculator*/
+/* exponent calculator */
 double power(double base, int exp) {
     double result = 1.0;
     for (int i = 0; i < exp; i++) {
@@ -13,169 +14,170 @@ double power(double base, int exp) {
     return result;
 }
 
-
-/* a manuel digit controller that control char and determine is it number by using ASCII*/
- int digit_control( char a){
-   return (a >= '0' && a <= '9');
- }
-
+/* a manual digit controller that controls char and determines if it is a number by using ASCII */
+int digit_control(char a) {
+    return (a >= '0' && a <= '9');
+}
 
 
 
-double evaluate_term(char *term, double x, double y) {
-    double result = 0.0;
-    int coef = 1; /* to make coefficent 1 if there has no coefficient*/
-    int exp_x = 0, exp_y = 0; /* exponent of x and y*/
-    
+/* parses a single term to determine coefficient and exponents of x and y */
+void parse_term(char *term, int *coef, int *exp_x, int *exp_y) {
+    *coef = 1;  
+    *exp_x = 0;  /*defaults*/
+    *exp_y = 0;  
+
     char *ptr = term;
+
+    while (*ptr==' ') ptr++; /*to skip spaces between two terms or terms between sign*/
+
     
-      /*to make coefficent -1 or +1 if there are -or + before that*/
-       if (*ptr == '-') {
-        coef = -1;
+
+    if (*ptr == '-') {
+        *coef = -1;
         ptr++;
+    while (*ptr==' ') ptr++; /*to skip spaces*/
+
+    
+
     } else if (*ptr == '+') {
-        ptr++;  /* continue*/
+        ptr++;
+        while (*ptr==' ') ptr++; /*to skip spaces*/
+
     }
-    
-   /*detect coefficient */
+
     int num = 0;
-    while (digit_control(*ptr)) {  
-        num = num * 10 + (*ptr - '0'); /*change char numbers to integer by using ASCII values. For example: Difference ascıı values between '0' and '2' = 2*/
+
+
+
+    while (digit_control(*ptr)) { /*control*/
+        num = num * 10 + (*ptr - '0');  /* change char numbers to integer by using ASCII values */
         ptr++;
     }
-    
     if (num != 0) {
-        coef = num;  
-    } else if (*ptr != 'x' && *ptr != 'y') {
-        coef = coef;  /*if there is no coefficient, it is 1 or -1 that we select before*/
+        *coef *= num;
     }
+
 
 /*x*/
     if (*ptr == 'x') {
-        exp_x = 1;  /*when we find x exponenet default 1*/
-        ptr++;  
-        if (*ptr == '^') {  
-            ptr++;  
+        *exp_x = 1;  /*if there has no coefficient its 1 as a default*/
+        ptr++;
+        if (*ptr == '^') {
+            ptr++;
             num = 0;
-            while (digit_control(*ptr)) { /*read exponent*/
+            while (digit_control(*ptr)) {
                 num = num * 10 + (*ptr - '0');
                 ptr++;
             }
-            exp_x = num;  // x'in üssü
+            *exp_x = num;
         }
     }
 
-   /*y*/
-    if (*ptr == 'y') {
-        exp_y = 1; 
-        ptr++;  
+/*y*/
+    if (*ptr == 'y') {  /*if there has no coefficient its 1 as a default*/
+        *exp_y = 1;  
+        ptr++;
         if (*ptr == '^') {
-            ptr++; 
+            ptr++;
             num = 0;
-            while (digit_control(*ptr)) {  
+            while (digit_control(*ptr)) {
                 num = num * 10 + (*ptr - '0');
                 ptr++;
             }
-            exp_y = num; 
+            *exp_y = num;
         }
     }
-    
-   /*calculations*/
-    result = coef * power(x, exp_x) * power(y, exp_y);
-    
-    return result;
 }
 
 
 
+/* function to calculate the result of a one term*/
+double calculate_term(int coef, int exp_x, int exp_y, double x, double y) {
+    return coef * power(x, exp_x) * power(y, exp_y);
+}
 
-/*divides polynoms to terms*/
-void process_polynomial(char *polynomial, double *total_result, double x, double y) {
-    int sign = 1;  
-    char term[100];  /*her terimi ayrı ayrı tutan dizi*/
-    int index = 0;   /* term için index */
 
-    int i;
-    for (i = 0; i <= strlen(polynomial); i++) {
+/* calculate all line*/
+double evaluate_polynomial(int coefs[], int exp_x[], int exp_y[], int term_count, double x, double y) {
+    double total_result = 0.0;
+    for (int i = 0; i < term_count; i++) {
+        total_result += calculate_term(coefs[i], exp_x[i], exp_y[i], x, y);  /*adds each other the results of terms*/
+    }
+    return total_result;
+}
+
+/* Divides polynomial into terms and returns the number of terms found */
+int split_polynomial(char *polynomial, char terms[MAX_TERMS][100]) {
+    int index = 0;
+    int term_count = 0;
+
+    for (int i = 0; i <= strlen(polynomial); i++) {
         if (polynomial[i] == '+' || polynomial[i] == '-' || polynomial[i] == '\0') {
             if (index > 0) {
-                term[index] = '\0';  /* finish term*/
-
-               /* calculate total term*/
-                *total_result += evaluate_term(term, x, y) * sign;
-
-                index = 0; /* make term index 0*/
+                terms[term_count][index] = '\0';  /* save terms for analyise then calculate (next function)*/
+                term_count++;
+                index = 0;
             }
-
-            // Yeni işareti ayarla
-            sign = (polynomial[i] == '-') ? -1 : 1;
+            if (polynomial[i] != '\0') {
+                terms[term_count][index++] = polynomial[i];  /*start new term*/
+            }
         } else {
-            term[index++] = polynomial[i];  // Terimi buffer’a ekle
+            terms[term_count][index++] = polynomial[i];  
         }
     }
-
-    printf("Polinomun toplam sonucu: %lf\n", *total_result);
+    return term_count;  /*founded terms number*/
 }
+
 
 
 int main() {
     FILE *polinom = fopen("polinom.txt", "r");
     if (!polinom) {
-        perror("File can not open polinom\n");
+        perror("File cannot be opened: polinom.txt\n");
         return 1;
     }
+
+    FILE *evaluations = fopen("evaluations.txt", "w");
+    if (!evaluations) {
+        perror("File cannot be opened: evaluations.txt\n");
+        fclose(polinom);
+        return 1;
+    }
+
+    char polynomial[MAX_LINE_LENGTH];
+    char terms[MAX_TERMS][100];
+    int coefs[MAX_TERMS], exp_x[MAX_TERMS], exp_y[MAX_TERMS]; 
+
+    while (fgets(polynomial, sizeof(polynomial), polinom)) { /*get polinom and save polynomial array*/
+        int term_count = split_polynomial(polynomial, terms); /*split polinom and parse to terms then save it to use later*/
+
+
+        /*parse, analyise and save as like a variable */
+        for (int i = 0; i < term_count; i++) {
+            parse_term(terms[i], &coefs[i], &exp_x[i], &exp_y[i]);
+        }
 
         FILE *values = fopen("values.txt", "r");
-    if (!values) {
-        perror("File can not open  values\n");
-        return 1;
+        if (!values) {
+            perror("File cannot be opened: values.txt\n");
+            fclose(polinom);
+            fclose(evaluations);
+            return 1;
+        }
+
+        double x, y;
+        while (fscanf(values, "%lf %lf", &x, &y) == 2) { /*control x and values readed true ?*/
+            double result = evaluate_polynomial(coefs, exp_x, exp_y, term_count, x, y);  /*calculate*/
+            fprintf(evaluations, "%.2lf ", result);  
+        }
+        fprintf(evaluations, "\n");
+
+        fclose(values); 
     }
 
-        FILE *evaluations = fopen("evaluations.txt", "r");
-    if (!values) {
-        perror("File can not open  evaluations\n");
-        return 1;
-    }    
-    
-    
-    char line[MAX_LINE_LENGTH];
-    
-    /*
-    double x, y;
-    printf("x değeri: ");
-    scanf("%lf", &x);
-    printf("y değeri: ");
-    scanf("%lf", &y);  */
-
-        double *total_result;
-
-
-
-    double x, y;
-    while (fscanf(polinom, "%lf %lf", &x, &y) == 2) { // İki değeri de başarıyla okuduğunda döner
- 
-    
-
-                /*we will read each line*/ 
-            while (fgets(line, sizeof(line), polinom)) {
-
-                       /* line = polynomial*/
-                 process_polynomial(line, total_result, x, y);
-                
-                       fprintf(evaluations, "%lf ", *total_result); /*prtint to eevulations calculatet polinoms*/
-                         /**/
-                       total_result=0;
-                            }
-
-                                                   fprintf(evaluations, "\n ");
- 
-
-    }
-    
     fclose(polinom);
-    fclose(values);
     fclose(evaluations);
 
-    
     return 0;
 }
